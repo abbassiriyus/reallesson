38fa721d72a38eb697faef0202780ed50c4c42ab
siriyusabbos <siriyusabbos@gmail.com>
2023-06-01 13:55:00 +0500

    s
    s

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './Login.css'
+import img from "./img/logo.png"
 export default class Login extends Component {
   render() {
     return (
         <div className="asosiy">
         <div className="logo">
-        <img src="" alt="" />
+        <img src={img} alt="" />
         <img src="" alt="" className="katta" />
         </div>
         <div className="form">
