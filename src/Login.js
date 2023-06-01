e2f825050351231d53d9af829a3715ae510e1d9d
siriyusabbos <siriyusabbos@gmail.com>
2023-06-01 13:53:31 +0500

    sf
    sdf

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './Login.css'
+export default class Login extends Component {
+  render() {
+    return (
+        <div className="asosiy">
+        <div className="logo">
+        <img src="" alt="" />
+        <img src="" alt="" className="katta" />
+        </div>
+        <div className="form">
+            <div className="all_button">
+                <button>Вход</button>
+                <button>Регистрация</button>
+            </div>
+            <div className="asos_form">
+<div className="login"></div>
+<div className="registratsiya"></div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
